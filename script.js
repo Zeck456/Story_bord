fc4e06c06be4b7fe9827fccb892ad88d345e14f3
Zeck456 <132918595+Zeck456@users.noreply.github.com>
2025-06-09 15:29:32 +0000

    .

--- script.js
+++ script.js
@@ -6,67 +6,71 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "1. O que você imagina para o seu futuro após o ensino médio?",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
+                texto: "Quero seguir para a universidade para obter um diploma.",
+                afirmacao: "Buscar uma formação acadêmica é uma escolha que visa a especialização e a preparação para o mercado de trabalho em áreas específicas. Isso também pode abrir portas para empregos com melhores salários e oportunidades de crescimento profissional "
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "Quis saber como usar IA no seu dia a dia."
+                texto: " Pretendo entrar no mercado de trabalho o mais rápido possível.",
+                afirmacao: " Escolher entrar no mercado de trabalho após o ensino médio pode oferecer uma experiência prática valiosa, permitindo que a pessoa desenvolva habilidades imediatamente e comece a construir uma carreira, sem a necessidade de um diploma universitário. "
             }
         ]
     },
     {
-        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
+        enunciado: "2. Como você acredita que as experiências do ensino médio vão impactar seu futuro?",
         alternativas: [
             {
-                texto: "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-                afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
+                texto: " As experiências sociais e acadêmicas do ensino médio me prepararam para os desafios da vida adulta.",
+                afirmacao: "O ensino médio oferece uma base sólida para o futuro, com experiências tanto acadêmicas quanto sociais que ajudam a moldar a capacidade de lidar com desafios profissionais e pessoais na vida adulta."
             },
             {
-                texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-                afirmacao: "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho."
+                texto: "Eu sinto que a escola foi mais uma fase que agora fica para trás, sem muitos impactos no meu futuro.",
+                afirmacao: "Para algumas pessoas, o ensino médio pode parecer apenas uma etapa necessária para seguir em frente. A experiência pode ser vista como algo passageiro, sem um impacto tão significativo nas escolhas futuras, especialmente se essas escolhas envolverem áreas muito diferentes da educação formal."
             }
         ]
     },
     {
-        enunciado: "Após a elaboração do trabalho escrito, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        enunciado: "3. Qual é a principal motivação que você tem para escolher seu caminho após o ensino médio?",
         alternativas: [
             {
-                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
-                afirmacao: "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA."
+                texto: "Quero conquistar estabilidade financeira e construir uma carreira sólida.",
+                afirmacao: "Buscar a estabilidade financeira logo após o ensino médio é uma prioridade para muitos, e isso pode ser alcançado por meio de escolhas profissionais estratégicas, como um curso técnico ou um emprego em uma área com alta demanda."
             },
             {
-                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
-                afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+                texto: "Busco realização pessoal e a possibilidade de explorar minha paixão por algo específico.",
+                afirmacao: "Para outros, o futuro é mais sobre encontrar um propósito ou paixão, o que pode levá-los a seguir carreiras ou estudar áreas que os fascinem, mesmo que isso envolva mais riscos ou um caminho menos previsível."
             }
         ]
     },
     {
-        enunciado: "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+        enunciado: "4. Como você se sente em relação ao risco de não saber o que fazer após o ensino médio?",
         alternativas: [
             {
-                texto: "Criar uma imagem utilizando uma plataforma de design como o Paint.",
-                afirmacao: "Notou também que muitas pessoas não sabem ainda utilizar as ferramentas tradicionais e decidiu compartilhar seus conhecimentos de design utilizando ferramentas de pintura digital para iniciantes."
+                texto: "Sinto que a pressão de decidir logo é muito grande, mas estou tentando me preparar de qualquer forma.",
+                afirmacao: " A pressão para decidir rapidamente o que fazer após o ensino médio pode ser um grande estresse, mas algumas pessoas tentam reduzir esse peso preparando-se de forma geral, seja buscando cursos extras, estágios ou experiências profissionais."
             },
             {
-                texto: "Criar uma imagem utilizando um gerador de imagem de IA.",
-                afirmacao: "Acelerou o processo de criação de trabalhos utilizando geradores de imagem e agora consegue ensinar pessoas que sentem dificuldades em desenhar manualmente como utilizar também!"
+                texto: "Não me preocupo com isso; tenho certeza de que vou encontrar um caminho no meu tempo.",
+                afirmacao: "Para quem não sente pressa, o tempo é visto como aliado. A ideia de que as oportunidades surgirão naturalmente permite uma abordagem mais tranquila e sem pressões externas, focando em encontrar o melhor caminho de forma orgânica."
             }
         ]
     },
     {
-        enunciado: "Você tem um trabalho em grupo de biologia para entregar na semana seguinte, o andamento do trabalho está um pouco atrasado e uma pessoa do seu grupo decidiu fazer com ajuda da IA. O problema é que o trabalho está totalmente igual ao do chat. O que você faz? ",
+        enunciado: "5. Você acredita que o que você aprenderá após o ensino médio será mais importante do que o que aprendeu na escola?",
         alternativas: [
             {
-                texto: "Escrever comandos para o chat é uma forma de contribuir com o trabalho, por isso não é um problema utilizar o texto inteiro.",
-                afirmacao: "Infelizmente passou a utilizar a IA para fazer todas suas tarefas e agora se sente dependente da IA para tudo."
+                texto: "Sim, acho que a faculdade ou o trabalho será onde realmente desenvolvo minhas habilidades e aprendo o que importa.",
+                afirmacao: "Muitas pessoas acreditam que o ensino formal, seja na faculdade ou em um ambiente de trabalho, é onde realmente se aprende as habilidades práticas e técnicas que são essenciais para a vida profissional, superando a educação mais genérica do ensino médio.
+
+."
             },
             {
-                texto: "O chat pode ser uma tecnologia muito avançada, mas é preciso manter a atenção pois toda máquina erra, por isso revisar o trabalho e contribuir com as perspectivas pessoais é essencial.",
-                afirmacao: "Percebeu que toda IA reproduz orientações baseadas na empresa que programou e muito do que o chat escrevia não refletia o que pensava e por isso sabe que os textos gerados pela IA devem servir como auxílio e não resultado final. "
+                texto: "Não, eu acredito que a base do ensino médio já me preparou o suficiente para o que virá.",
+                afirmacao: "Alguns acreditam que a base que o ensino médio oferece é o suficiente para a vida adulta, considerando que o aprendizado ali adquirido é essencial e que a adaptação ao mercado de trabalho será mais uma questão de vivência do que de novos conhecimentos acadêmicos.
+
+ "
             }
         ]
     },
